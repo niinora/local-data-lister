dd6f24d4efe94cad4fdac6db6fb54936921449b7
kiu_kristesiashvili.tama <kristesiashvili.tama@kiu.edu.ge>
2025-06-28 16:02:10 +0400

    Implement E2E Tests with Authentication

---
+++ client/cypress.config.ts
@@ -0,0 +1,26 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        }
+      });
+      return config;
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
+    excludeSpecPattern: [
+      '**/examples/*',
+      '**/1-getting-started/*',
+      '**/2-advanced-examples/*'
+    ]
+  }
+});
--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const cors = require('cors')
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  next();
+});
 
 const itemSchema = Joi.object({
   name: Joi.string().required(),
